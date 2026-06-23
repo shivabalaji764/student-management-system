a5d53b3b245f7bc42012940736c5b3af0211a48a
shivabalaji764 <shivatalla764@gmail.com>
2026-06-23 09:43:21 +0530

    final commit

---
+++ student-management-system/src/main/java/com/example/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
+
+}
